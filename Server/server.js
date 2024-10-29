5e251112ab51f220d9422709a4e8cfb9155f8e0f
DevonRem <DevonReminick@gmail.com>
2024-10-29 13:52:36 -0400

    SocketIO functionality finished, each player has respective square, multiplayer functionality working

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const clientLandingPage = path.join(__dirname, '../','/client/index.html');
+const clientLandingPage = path.join(__dirname, '../','/Client/index.html');
 app.get('/', (req, res)=> {
     res.sendFile(clientLandingPage)
 });
@@ -15,10 +15,23 @@ server.listen(2000, () => {
     console.log('server is running on port 2000');
 });
 
+let players = {};
+
 io.on('connection', (socket) => {
     console.log('user connected');
+    players[socket.id] = { x: 0, y: 0};
+
+    socket.on('move', (data) => {
+        players[socket.id].x += data.dx;
+        players[socket.id].y += data.dy;
+    });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
+
+let framerate = 30;
+setInterval(function() {
+    io.emit('state', players);
+}, 1000/framerate);
